9fb59cbad18ec1d29a07465bfb74ee3f42997440
Christine Wu <christinewu9230@gmail.com>
2022-02-11 14:40:50 -0800

    fix file organizations

--- LaiOffer/src/DP/Main.java
+++
@@ -1,8 +0,0 @@
-package DP;
-
-public class Main {
-    public static void main(String[] args) {
-        Linear l = new Linear();
-        l.test();
-    }
-}
--- LaiOffer/src/StringQs/Main.java
+++
@@ -1,8 +0,0 @@
-package StringQs;
-
-public class Main {
-    public static void main(String[] args) {
-        StringII str2 = new StringII();
-        str2.test();
-    }
-}
